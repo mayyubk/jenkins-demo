25bce109115127a92bea61e255e1bd575ebc6dd3
ayyub <contactayyub@gmail.com>
2025-08-13 10:40:58 +0500

    this is 3rd Update

--- build.py
+++ build.py
@@ -2,3 +2,4 @@ print("Jenkins auto-build triggered from GitHub!")
 import datetime 
 print("Current date and time:", datetime.datetime.now()) 
 print ("this is second update")
+print("this is 3rd Update")
